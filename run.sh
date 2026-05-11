0bfcef6afbd10d74e53d72faae085457ae6706ce
정진우 <wjdwlsuuu@jungjinuuu.local>
2026-05-11 10:11:21 +0900

    Update Project Files

--- run.sh
+++ run.sh
@@ -1,10 +1,1 @@
-conda activate comp
-
-python src/models/baseline.py
-python src/models/tree_models.py
-python src/models/optuna.py
-python src/models/catboost_optuna.py --n-trials 40
-python src/models/stacking.py
-
-python src/models/train_final_model.py
-python src/frontend/app.py --host 0.0.0.0 --port 8501
+python src/frontend/app.py
